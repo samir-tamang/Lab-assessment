c54b8926329cc080072fbeaf7ea5d389540bb6c3
Samir Bolan <77658596+samir-tamang@users.noreply.github.com>
2024-12-25 12:53:23 +0000

    week4

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import GreetingElement from './mygreetingApp';
-import GreetingElementwithProps from './mygreetingprops'
+var express = require ("express") 
+var fs=require("fs")
+var app=express()
+// add middle ware function for body parsing
+var bodyParser= require ("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <GreetingElement />
-    <GreetingElementwithProps/>
-  </React.StrictMode>
-);
+app.get ('/',function(req,res){
+    res.send("hello it is my irst express application")
+})
+app.get('/about', function(req,res){
+    res.send("This is a baic express application")
+})
+app.get('/users/:userId/books/:bookId', function (req,res){
+    res.send(req.params)
+})
+app.get('/GetStudents',function (req,res)
+{studentdata={}
+    fs.readFile(__dirname + "/"+ "student.json", 'utf8',
+        function (err,data) {console.log(data);
+            res.json ({'status':true, 'Status_Code':200, 'requested at': req.localtime,
+                'requrl':req.url,
+                'request Method': req.method, 'studentdata': JSON.parse(
+                    data)});
+        });
+})
+app.get ('/GetStudentid/:id',(req,res)=>{studentdata={}
+fs.readFile(__dirname + "/" + "Student.json", 'utf8'
+    ,function (err,data) {
+        var students = JSON.parse (data)
+        var student =students["student"+req.params.id]
+        console.log ("student", student)
+        if (student)
+            res.json (student)
+        else
+        res.json ({'status': true, 'Status_code':200, 'requested at': req.localtime,
+            'requrl':req.url, 'request Method':req.method, 'studentdata':JSON.parse(data)});
+    });
+})
+app.get ('/studentinfo', function(req,res)
+{
+    res.sendFile('StudentInfo.html', {
+        root: __dirname
+    });
+})
+app.post('/submit-data', function (req,res){
+    var name= req.body.firstName + '' +req.body.lastName+'';
+    var Age= req.body.myAge+ 'Gender:' + req.body.gender+''
+    Qual= 'Qualification'+ req.body.Qual
+    console.log(req.body.Qual)
+    res.send({ status:true,
+        message: 'form Details',data:{
+            name: name, age: Age, Qualification:Qual,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        } });   });
+app.listen (5000,function(){console.log("server is runningpn port 5000")})
